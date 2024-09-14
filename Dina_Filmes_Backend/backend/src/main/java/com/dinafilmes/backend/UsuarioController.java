38d512f6b91d5d80b125305f80fca3e7f8db5752
Rosana <rosana.della.bruna@gmail.com>
2024-09-13 21:23:18 -0300

    inserindo a página tela-filmes

--- Dina_Filmes_Backend/backend/src/main/java/com/dinafilmes/backend/UsuarioController.java
+++ Dina_Filmes_Backend/backend/src/main/java/com/dinafilmes/backend/UsuarioController.java
@@ -1,14 +1,17 @@
 package com.dinafilmes.backend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
+import java.util.Date;
 
 @RestController
 @CrossOrigin("*")
@@ -41,5 +44,35 @@ public ResponseEntity<Boolean> verificarEmail(@RequestParam String email) {
     boolean exists = repository.existsByEmail(email);
     return ResponseEntity.ok(exists);
 }
+
+    @PutMapping("/api/usuario")
+    public ResponseEntity<String> alterar(@RequestBody UsuarioEntity obj) {
+        try {
+            // Verifica se o código do usuário é válido
+            if (obj.getCodigoUsuario() <= 0) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Código do usuário inválido");
+            }
+
+            // Busca o usuário existente
+            Optional<UsuarioEntity> usuarioOptional = repository.findById(obj.getCodigoUsuario());
+
+            if (!usuarioOptional.isPresent()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+            }
+
+            UsuarioEntity usuarioExistente = usuarioOptional.get();
+
+            // Atualiza somente o campo nomeUsuario
+            usuarioExistente.setNomeUsuario(obj.getNomeUsuario());
+            //usuarioExistente.setDataAtualizacao(new Date()); // Atualiza a data de modificação
+
+            // Salva a atualização
+            repository.save(usuarioExistente);
+            return ResponseEntity.ok("Cadastro atualizado");
+        } catch (Exception e) {
+            e.printStackTrace(); // Log da exceção para depuração
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar o cadastro");
+        }
+    }
     
 }
