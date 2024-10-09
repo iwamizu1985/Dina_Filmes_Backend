43cf3f06206a5c220714d2e57f41960e98ab699f
Rosana <rosana.della.bruna@gmail.com>
2024-10-09 13:24:23 -0300

    adicionar a favoritos ainda com um bug no desfavoritar

--- src/main/java/com/dinafilmes/backend/ListaRepository.java
+++ src/main/java/com/dinafilmes/backend/ListaRepository.java
@@ -14,9 +14,11 @@ public interface ListaRepository extends JpaRepository<ListaEntity, Integer> {
     List<FilmeEntity> carregarListaFavoritos(int codigoUsuario);
 
 
+    @Query("SELECT l FROM ListaEntity l WHERE l.codigoUsuario = :codigoUsuario AND l.codigoFilme = :codigoFilme AND l.filmeFavorito = true")
+Optional<ListaEntity> verificarFilmeFavorito(@Param("codigoUsuario") int codigoUsuario, @Param("codigoFilme") int codigoFilme);
 
-    @Query("SELECT l FROM ListaEntity l WHERE l.codigoUsuario = ?1 AND l.codigoFilme = ?2")
-    Optional<ListaEntity> verificarFilmeFavorito(int codigoUsuario, int codigoFilme);
+    // @Query("SELECT l FROM ListaEntity l WHERE l.codigoUsuario = ?1 AND l.codigoFilme = ?2")
+    // Optional<ListaEntity> verificarFilmeFavorito(int codigoUsuario, int codigoFilme);
 
     
     @Query("SELECT f FROM FilmeEntity f JOIN ListaEntity l ON f.codigoFilme = l.codigoFilme WHERE l.codigoUsuario = ?1 AND l.filmeAssistido = true")
